3bf5b6f7b83db0c647b3feb526367475e9475f68
872867 <872867@PC413731.cts.com>
2021-01-07 10:47:17 +0530

    DriverSetup final Commit

--- Parabank/src/main/java/com/testing/Parabank/TestBase/DriverSetup.java
+++ Parabank/src/main/java/com/testing/Parabank/TestBase/DriverSetup.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.opera.OperaDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class DriverSetup {
 	//Declaring the static variable
 	public static WebDriver driver;
@@ -15,49 +17,23 @@ public class DriverSetup {
 		
 		//Invoking Chrome Driver in WindowsOS
 		if (browserName.equalsIgnoreCase("ChromeDriver_WindowsOS")) {
-			String chromeDriverPath = "\\Drivers\\chromedriver.exe";
-			String path=System.getProperty("user.dir");
-			System.setProperty("webdriver.chrome.driver", path+chromeDriverPath);
-			
+			WebDriverManager.chromedriver().setup();
 			driver= new ChromeDriver();	
 			driver.get("https://parabank.parasoft.com/parabank/index.htm");
 		}
 		//Invoking Firefox Driver in WindowsOS
 		else if (browserName.equalsIgnoreCase("FirefoxDriver_WindowsOS")) {
-			String firefoxDriverPath="\\Drivers\\geckodriver.exe";
-			String path= System.getProperty("user.dir");
-			System.setProperty("webdriver.gecko.driver", path+firefoxDriverPath);
-			
+			WebDriverManager.firefoxdriver().setup();
 			driver= new FirefoxDriver();
 			driver.get("https://parabank.parasoft.com/parabank/index.htm");
 		}
 		//Invoking Opera Driver in WindowsOS
 		else if(browserName.equalsIgnoreCase("OperaDriver_WindowsOS")) {
-			String operaDriverPath="\\Drivers\\operadriver.exe";
-			String path= System.getProperty("user.dir");
-			System.setProperty("webdriver.opera.driver", path+operaDriverPath);
-			
+			WebDriverManager.operadriver().setup();
 			driver= new OperaDriver();
 			driver.get("https://parabank.parasoft.com/parabank/index.htm");
 		}
-		//Invoking Chrome Driver in MacOS
-		else if (browserName.equalsIgnoreCase("ChromeDriver_MacOS")) {
-			String chromeDriverPath = "\\Drivers\\chromedriver";
-			String path=System.getProperty("user.dir");
-			System.setProperty("webdriver.chrome.driver", path+chromeDriverPath);
-			
-			driver= new ChromeDriver();	
-			driver.get("https://parabank.parasoft.com/parabank/index.htm");
-		}
-		//Invoking Opera Driver in MacOS
-		else if(browserName.equalsIgnoreCase("OperaDriver_MacOS")) {
-			String operaDriverPath="\\Drivers\\operadriver";
-			String path= System.getProperty("user.dir");
-			System.setProperty("webdriver.opera.driver", path+operaDriverPath);
-			
-			driver= new OperaDriver();
-			driver.get("https://parabank.parasoft.com/parabank/index.htm");
-		}
+		
 		//Returning the Driver
 		return driver;
 	}
